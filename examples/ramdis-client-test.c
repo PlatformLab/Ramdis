a17060a0e81138dbc882a7968a7a78a6131115ea
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-12-09 15:13:03 -0800

    Simple client to test GET and SET.

--- examples/ramdis-client-test.c
+++ examples/ramdis-client-test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "ramdis.h"
 
@@ -7,6 +8,20 @@ int main(int argc, char* argv[]) {
   printf("Ramdis Client Test\n");
   printf("Connecting to %s\n", argv[1]);
   void* context = connect(argv[1]); 
-  ping(context, "Hi Ramdis!");
+
+  Object key;
+  key.data = "Bobby Jones";
+  key.len = strlen(key.data)+1;
+  Object value;
+  value.data = "Age: 28, Occupation: lawyer, Trophies: 4";
+  value.len = strlen(value.data)+1;
+  set(context, &key, &value);
+
+  Object* obj = get(context, &key);
+  printf("key/value: (%s) : (%s)\n", (char*)key.data, (char*)obj->data);
+
+  freeObject(obj);
+
+  disconnect(context);
   return 0;
 }
