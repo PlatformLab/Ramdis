cf60869ff5dc320071b701a5a57c54a2fe2793e3
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-01-19 10:23:11 -0800

    Moved data file processing to separate script.
    Now RamdisPerf.py is only responsible for running the experiment.
    MakeGNUPlotFiles.py is responsible for looking at a directory full of
    files generated from experiments and generating / updating the gnuplot
    files for plotting by gnuplot scripts.

--- ramdis-benchmark/RamdisPerf.py
+++ ramdis-benchmark/RamdisPerf.py
@@ -7,47 +7,6 @@ import os
 import cluster
 from optparse import OptionParser
 import re
-
-def read_csv_into_list(filenameList):
-    """
-    Read csv files of floats, concatenate them all into a flat list and return
-    them.
-    """
-    numbers = []
-    for filename in filenameList:
-        for line in open(filename, 'r'):
-            if not re.match('([0-9]+\.[0-9]+) ', line):
-                for value in line.split(","):
-                    numbers.append(float(value))
-    return numbers
-
-def gen_cdf(filenameList, outputFilename):
-    """
-    Read data values from fileis given by filenameList, and produces a cdf in
-    text form.  Each line in the printed output will contain a fraction and a
-    number, such that the given fraction of all numbers in the log file have
-    values less than or equal to the given number.
-    """
-    # Read the file into an array of numbers.
-    numbers = read_csv_into_list(filenameList)
-
-    # Output to the current file + .cdf
-    outfile = open(outputFilename, 'w')
-
-    # Generate a CDF from the array.
-    numbers.sort()
-    result = []
-    outfile.write("%8.4f    %8.3f\n" % (0.0, 0.0))
-    outfile.write("%8.4f    %8.3f\n" % (numbers[0], 1/len(numbers)))
-    for i in range(1, 100):
-        outfile.write("%8.4f    %8.3f\n" % (numbers[int(len(numbers)*i/100)], 
-            i/100))
-    outfile.write("%8.4f    %8.3f\n" % (numbers[int(len(numbers)*999/1000)], 
-        .999))
-    outfile.write("%8.4f    %9.4f\n" % (numbers[int(len(numbers)*9999/10000)], 
-        .9999))
-    outfile.write("%8.4f    %8.3f\n" % (numbers[-1], 1.0))
-    outfile.close()
     
 def flatten_args(args):
     """
@@ -209,44 +168,3 @@ if __name__ == '__main__':
             cluster.run(client="../ramdis-benchmark/ramdis-benchmark %s" % (flatten_args(client_args)), **cluster_args)
 
             print ""
-
-    # Parse output files and generate graphs
-    for test in options.tests.split(','):
-        tvcfile = open("%s/%s_throughput_v_clients.dat" 
-                % (options.output_dir, test), 'w')
-        for c in options.clients.split(','):
-            reqLatFilenameList = []
-            execSumFilenameList = []
-            for j in range(1, int(c) + 1):
-                reqLatFilenameList.append("%s/%s_client%d-%d_reqLatencies.dat" 
-                        % (options.output_dir, test, int(c), j))
-                execSumFilenameList.append("%s/%s_client%d-%d_execSummary.dat" 
-                        % (options.output_dir, test, int(c), j))
-
-            # Output latency CDF
-            gen_cdf(reqLatFilenameList, "%s/%s_client%d-all_reqLatencies.cdf" 
-                    % (options.output_dir, test, int(c)))
-
-            # Output total throughput
-            maxTotalTime = 0.0
-            totalOps = 0
-            for filename in execSumFilenameList:
-                sumDict = {}
-                for line in open(filename, 'r'):
-                    cols = line.split(' ')
-                    sumDict[cols[0]] = cols[1]
-                
-                if float(sumDict['totalTime']) > maxTotalTime:
-                    maxTotalTime = float(sumDict['totalTime'])
-                    
-                totalOps += int(sumDict['totalOps'])
-
-            tputfile = open("%s/%s_client%d-all_throughput.dat" 
-                    % (options.output_dir, test, int(c)), 'w')
-            tputfile.write("%.2f\n" % (float(totalOps) / maxTotalTime))
-            tputfile.close()
-
-            tvcfile.write("%d %9.2f\n" 
-                    % (int(c), float(totalOps) / maxTotalTime))
-
-        tvcfile.close()
